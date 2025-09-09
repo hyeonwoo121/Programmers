58693bed6be480b61beb5556f16eda7b5818905c
이현우 <justice3350@naver.com>
2025-09-10 02:38:12 +0900

    Programmers 01문제

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.programmers;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -7,6 +7,12 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        //솔루션 클래스의 인스턴스를 생성
+        Solution sol = new Solution();
+
+        int result = sol.solution(10,2);
+
+        System.out.println("결과: "+ result);
        
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
